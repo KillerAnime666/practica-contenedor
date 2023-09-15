93d0d5c82c5eea7383b21e7d853ec31960fba3ea
Sergio Fernandez Davila <killeranime666@gmail.com>
2023-09-14 20:03:00 -0400

    Creacion del proyecto

---
+++ src/main/java/bo/edu/ucb/practicacontenedor/PracticaContenedorApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.practicacontenedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaContenedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaContenedorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/practicacontenedor/PracticaContenedorApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.practicacontenedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaContenedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
